2783ab6312f978bfaf61dc81f06fdd1bf893347f
alyalsayed <alyalsayed56@yahoo.com>
2024-08-14 01:53:46 +0300

    feat: update DELETE.php

--- employees/delete.php
+++ employees/delete.php
@@ -16,10 +16,32 @@ if (isset($_GET['ssn'])) {
     echo "<h1 align='center'>wrong page !!!!</h1>";
     exit();
 }
-$res = $connection->query("select image from employee where ssn=$ssn");
-$image = $res->fetch(PDO::FETCH_ASSOC);
-$image_name = $image["image"];
-$result = $connection->query("delete from employee where ssn=$ssn");
-unlink("../uploads/images/$image_name");
+
+// Fetch all images associated with the employee
+$image_query = "SELECT image_name FROM employee_images WHERE employee_ssn = :ssn";
+$image_stmt = $connection->prepare($image_query);
+$image_stmt->execute(['ssn' => $ssn]);
+$images = $image_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Delete employee images from the database
+$delete_images_query = "DELETE FROM employee_images WHERE employee_ssn = :ssn";
+$delete_images_stmt = $connection->prepare($delete_images_query);
+$delete_images_stmt->execute(['ssn' => $ssn]);
+
+// Unlink each image file from the filesystem
+foreach ($images as $image) {
+    $image_path = "../uploads/images/" . $image['image_name'];
+    if (file_exists($image_path)) {
+        unlink($image_path);
+    }
+}
+
+// Delete the employee record
+$delete_employee_query = "DELETE FROM employee WHERE ssn = :ssn";
+$delete_employee_stmt = $connection->prepare($delete_employee_query);
+$delete_employee_stmt->execute(['ssn' => $ssn]);
+
+// Redirect to the index page
 header("location: index.php");
+exit();
 ?>
